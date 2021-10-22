85477a5af3d54f656d7f69292412ae9458ea5402
AshikShuvo <ashikshuvo1996@gmail.com>
2021-10-22 10:00:49 +0600

    nav init

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home';
 
 function App() {
   return (
-    <h1>hello world</h1>
+    <Home/>
   );
 }
 
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,12 @@
+import React,{FC} from 'react'
+import NavBar from '../Components/NavBar'
+
+const Home : FC= () => {
+    return (
+        <div className="min-h-full">
+            <NavBar/>
+        </div>
+    )
+}
+
+export default Home
